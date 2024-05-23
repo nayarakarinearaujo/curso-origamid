2c1593f361dec28caf5a33499e2495c3f6bc92bd
-Nayara <nayarakarine.araujo@gmail.com>
2024-05-23 17:55:36 -0300

    aula de js

---
+++ javascript/aula01-variaveis/script.js
@@ -0,0 +1,31 @@
+window.alert("Seja bem vindo");
+
+var nome = 'Nayara';
+var idade = 30;
+var sobrenome = 'Araujo';
+
+console.log(nome, idade, sobrenome);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado + preco;
+
+console.log(totalPreco);
+
+var cidade = 'Campinas -';
+var estado = 'São Paulo';
+
+console.log(cidade, estado);
+
+var comida;
+console.log(comida);
+var comida = 'Pizza';
+
+var time= 'Santos';
+console.log(time);
+
+var professor, 
+escola, 
+pais, 
+endereco, 
+rua;
