22c6f59c84ac79fc125e38743a23053054080f1c
Nayara <nayarakarine.araujo@gmail.com>
2024-05-27 21:03:19 -0300

    JavaScript

---
+++ javascript/TiposDeDados/script.js
@@ -0,0 +1,13 @@
+var idade = 28;
+console.log(typeof idade);
+
+var none = 'Nayara';
+var sobrenome = 'Karine';
+var nomeCompleto = nome+ '' +sobrenome;
+console.log(nomeCompleto);
+
+var gols = 100;
+var frase = 'Romário fez ' + gols + 'gols';
+console.log(typeof frase);
+
+console.log
