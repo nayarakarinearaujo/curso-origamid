dfa136d8ff9e6ca6eedfd47d696b91cc8d144dbe
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-06-21 09:28:35 -0300

    Aula 21/06/24

---
+++ javascript/Objetos/script.js
@@ -0,0 +1,14 @@
+var pessoa = {
+    nome: 'Nayara',
+    sobrenome: 'Araújo',
+    idade: 31,
+    faculdade: 'True',
+    profissao:'Desenvolvedora',
+}
+
+var quadrado = {
+    lados: 4,
+    area: function(lado) {
+        return lado * lado;
+    }
+}
