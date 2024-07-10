9ff0f94a57f90436554c1b162148d8184f8522ed
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-07-10 12:12:32 -0300

    Aula dia 10/07/24

--- javascript/Dom/Exercicio-AnimaisFantasticos/script.js
+++ javascript/Dom/Exercicio-AnimaisFantasticos/script.js
@@ -1,11 +1,28 @@
 
 const animais = document.getElementById('animais');
+console.log(animais);
 
 const gridSection = document.getElementsByClassName('grid-section');
+console.log(gridSection);
 
 
 const primeiraLi = document.querySelector('li');
+console.log(primeiraLi);
 
 const linksInternos = document.querySelector('[href^="#]');
+console.log(linksInternos)
 
 const animaisImg = document.querySelectorAll('.animais img');
+console.log(animaisImg);
+
+
+const gridSectionHTML = document.getElementsByClassName('grid-section');
+
+const gridSectionNode = document.querySelectorAll('.grid-section');
+
+console.log(gridSectionHTML[0]);
+console.log(gridSectionNode[0]);
+
+gridSection.forEach(function(item, index){
+    console.log(item);
+});
