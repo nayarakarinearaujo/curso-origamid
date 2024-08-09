bd2f258f5adc4ecf069f54d5e71486f6e16657e3
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-08-09 09:07:13 -0300

    Objetos

---
+++ javascript/Aula006-Objetos/objetos03.js
@@ -0,0 +1,16 @@
+
+//! Tudo é um objeto
+
+var nome = 'Nayara';
+
+nome.length;
+nome.charAt(1);
+nome.replace('ya', 'ia');
+nome;
+
+//Contar os caractere
+console.log(nome.length);
+
+var nomeMinusculo = nome.toLocaleLowerCase();
+
+console.log(nomeMinusculo)
