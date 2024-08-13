81a1a8a7c5bb2513a9f4a8b5cb2b4198738914bd
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-08-13 11:31:29 -0300

    Aula ForEach e Arrow Function

--- javascript/Aula010-Dom/Aula013-ForEach/scriptAula.js
+++ javascript/Aula010-Dom/Aula013-ForEach/scriptAula.js
@@ -1,11 +1,12 @@
+
 //! Entendendo forEach
 
 const imgs = document.querySelectorAll('img');
 
 
-// 1° Forma de usar
-imgs.forEach(function (item,) {
-    console.log(item);
+// 1° Forma de usar callback
+imgs.forEach(function (item, index, array) {
+    console.log(item,index, array);
 });
 
 
@@ -15,6 +16,35 @@ imgs.forEach((item) => {
 });
 
 
-//3° Forma de fazer
+
+//! Entendendo Arrow Function
+
+/* Sintaxe curta em relação a function expression.
+Basta remover a palavra chave function e adicionar a
+fat arrow => após os argumentos
+
+* CASO TENHA APENAS UM ARGUMENTO PODE DEIXAR SEM AS CHAVES
 imgs.forEach((item) => console.log(item));
 
+*/
+
+//3° Forma de fazer  com Arrow Function
+imgs.forEach((item) => {
+    console.log(item)
+});
+
+
+//! Entendendo forEach e Array
+
+const titulos = document.getElementsByClassName('titulo');
+const titulosArray = Array.from(titulos);
+
+//* Retorna o htmlCollection
+console.log(titulos);
+
+//* Retorna o array de titulos
+console.log(titulosArray);
+
+titulosArray.forEach(function(item) {
+    console.log(item);
+})
