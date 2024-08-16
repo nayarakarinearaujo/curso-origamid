adfcc5dc725031a5eee1c9c0f9a30a5f49345cf3
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-08-16 10:43:21 -0300

    forEach e eventos

--- javascript/Aula010-Dom/Aula017-Eventos2/scriptAula.js
+++ javascript/Aula010-Dom/Aula017-Eventos2/scriptAula.js
@@ -2,3 +2,18 @@
 //! Eventos 2
 
 //* forEach e eventos
+/* O método addEventListener é adicionado á um único elemento, 
+então é necessário um loop entre elementos de uma lista, para adicionarmos
+á cada um deles.
+ */
+
+const imgs = document.querySelector('img');
+
+function imgSrc(event) {
+    const src = event.currentTarget.getAttribute('src');
+    console.log(src);
+}
+
+imgs.forEach((img) => {
+    img.addEventListener('click', imgSrc);
+});
