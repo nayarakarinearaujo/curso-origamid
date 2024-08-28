3baadbb3626636378272722b4087b8ac1bcefa08
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-08-28 10:46:00 -0300

    Aula dia 28/08/24

--- javascript/Aula010-Dom/Aula018-TraversingEManipulacao/transversing.js
+++ javascript/Aula010-Dom/Aula018-TraversingEManipulacao/transversing.js
@@ -14,3 +14,16 @@ lista.children[--lista.children.length]; // Retorna o último filho
 
 lista.querySelectorAll('li'); // Retorna todas as lis
 lista.querySelector('li:last-child'); //Retorna ultimo filho
+
+
+//! Manipulando elemento
+
+const contato = document.querySelector('.contato');
+const animais = document.querySelector('.animais');
+const titulo = contato.querySelector('.titulo');
+
+animais.appendChild(titulo); // nesse caso o titulo de contato vai ficar abaixo dos animais
+
+contato.insertBefore(lista, titulo);//insere a lista antes de titulo
+contato.removeChild(titulo); // remove titulo de contato
+contato.replaceChild(lista, titulo); // substitui titulo por lista
