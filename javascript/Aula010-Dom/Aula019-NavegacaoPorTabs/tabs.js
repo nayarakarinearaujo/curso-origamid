55cd43fc54699406106902e2f615c050ff0942ae
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-09-03 16:37:33 -0300

    Aula 03/09/24

--- javascript/Aula010-Dom/Aula019-NavegacaoPorTabs/tabs.js
+++ javascript/Aula010-Dom/Aula019-NavegacaoPorTabs/tabs.js
@@ -1,17 +1,28 @@
+// Seleciona todos os elementos <li> dentro do contêiner com a classe .js-tabmenu
 const tabMenu = document.querySelectorAll(".js-tabmenu li");
+// Seleciona todas as seções <section> dentro do contêiner com a classe .js-tabcontent
 const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-//Loop para cada section acrescentando o ativo
-function activeTab(index) {
-  tabContent.forEach((section) => {
-    section.classList.remove("ativo");
-  });
-  tabContent[index].classList.add("ativo");
-}
+// Verifica se há itens em tabMenu e tabContent antes de continuar
+if (tabMenu.length && tabContent) {
+  // Adiciona a classe "ativo" à primeira seção, tornando-a visível por padrão
+  tabContent[0].classList.add("ativo");
 
-//Loop por cada li
-tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-        activeTab(index);
-    })
-});
+  // Função para ativar a aba correspondente ao índice clicado
+  function activeTab(index) {
+    // Remove a classe "ativo" de todas as seções, ocultando-as
+    tabContent.forEach((section) => {
+      section.classList.remove("ativo");
+    });
+    // Adiciona a classe "ativo" à seção correspondente ao índice clicado, exibindo-a
+    tabContent[index].classList.add("ativo");
+  }
+
+  // Adiciona um evento de clique a cada item do menu (li)
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener("click", () => {
+      // Chama a função activeTab passando o índice da aba clicada
+      activeTab(index);
+    });
+  });
+}
