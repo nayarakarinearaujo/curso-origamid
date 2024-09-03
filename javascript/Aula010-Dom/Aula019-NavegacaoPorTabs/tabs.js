99e8b19a0871b2ce56dfa802a36170b18b514c24
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-09-03 15:50:15 -0300

    Naveção por tab

--- javascript/Aula010-Dom/Aula019-NavegacaoPorTabs/tabs.js
+++ javascript/Aula010-Dom/Aula019-NavegacaoPorTabs/tabs.js
@@ -1,4 +1,16 @@
+const tabMenu = document.querySelectorAll(".js-tabmenu li");
+const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+//Loop para cada li
+function activeTab(index) {
+  tabContent.forEach((section) => {
+    section.classList.remove("ativo");
+  });
+  tabContent[index].classList.add("ativo");
+}
 
+tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => {
+        activeTab(index);
+    })
+});
