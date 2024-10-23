5f46f1ebc9c94dd33d1db9311c34233b19737842
nayarakarine.araujo@gmail.com <nayarakarine.araujo@gmail.com>
2024-10-23 20:04:50 -0300

    Native

--- javascript/Aula023-Objetos/Aula026-Native-Host-User/native.js
+++ javascript/Aula023-Objetos/Aula026-Native-Host-User/native.js
@@ -4,4 +4,10 @@
 Objetos nativos são aqueles definidos na especificação
 da linguagem e são implementados indenpendente do hots.
 
+Construtores de objetos nativos
+-Object
+-String
+-Array
+-Function
 */
+
