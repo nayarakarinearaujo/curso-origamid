c7266d908dca155e21eb5cc4a0c9b814338824de
nayarakarine <nayarakarine.araujo@gmail.com>
2025-06-17 11:33:11 -0300

    feat: (Promise)
    - Promise;

--- javascript/AulaAssincrono/001Promise/script.js
+++ javascript/AulaAssincrono/001Promise/script.js
@@ -1,44 +1,49 @@
 // const promesa = new Promise((resolve, reject) => {
-//   let condicao = false;
-//   if(condicao) {
-//     setTimeout(()=>{
-//       resolve({nome: 'Andre', idade: 28});
-//     }, 1000)
+//   let condicao = true;
+//   if (condicao) {
+//     setTimeout(() => {
+//       resolve({ nome: "Andre", idade: 28 });
+//     }, 1000);
 //   } else {
-//     reject(Error('Um erro ocorreu na promise'))
+//     reject(Error("Um erro ocorreu na promise"));
 //   }
 // });
 
 // const retorno = promesa
-// .then(resolucao => {
-//   console.log(resolucao);
-//   resolucao.profissao = 'Designer';
-//   return resolucao;
-// })
-// .then(resolucao => {
-//   console.log(resolucao)
-// }, rejeitada => {
-//   console.log(rejeitada);
-// }).finally((teste) => {
-//   console.log(teste)
-// })
+//   .then((resolucao) => {
+//     console.log("Primeiro then:", resolucao);
+//     resolucao.profissao = "Designer";
+//     return resolucao;
+//   })
+//   .then((resolucao) => {
+//     console.log("Segundo then:", resolucao);
+//   })
+//   .finally(() => {
+//     //Sempre é executado
+//     console.log("Finalizou (sucesso ou erro)");
+//   })
+//   //Executado caso der erro
+//   .catch((rejeitado) => {
+//     console.log("Erro capturado no catch:", rejeitado.message);
+//   });
 
-// console.log(retorno);
+// console.log("Retorno imediato da promise:", retorno);
 
 const login = new Promise((resolve) => {
   setTimeout(() => {
-    resolve('Usuário Logado')
-  }, 1000)
-})
+    resolve("Usuário Logado");
+  }, 1000);
+});
 
 const dados = new Promise((resolve) => {
   setTimeout(() => {
-    resolve('Dados Carregados')
-  }, 1500)
-})
+    resolve("Dados Carregados");
+  }, 1500);
+});
 
-const carregouTudo = Promise.race([login, dados]);
+const carregouTudo = Promise.all([login, dados]);
 
 carregouTudo.then((resolucao) => {
   console.log(resolucao);
-})
+});
+console.log(carregouTudo);
