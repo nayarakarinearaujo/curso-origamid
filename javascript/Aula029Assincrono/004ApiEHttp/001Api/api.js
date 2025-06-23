62bf94805b42a72eaee815fac27b1a9a46882c98
nayarakarine <nayarakarine.araujo@gmail.com>
2025-06-23 20:41:51 -0300

    feat: (JavaScript )
    - API;

--- javascript/Aula029Assincrono/004ApiEHttp/001Api/api.js
+++ javascript/Aula029Assincrono/004ApiEHttp/001Api/api.js
@@ -1,1 +1,22 @@
 //* API
+
+// fetch('https://pokeapi.co/api/v2/pokemon/1/')
+// .then(r => r.json())
+// .then(pokemon => {
+//  console.log(pokemon);
+// });
+
+const url = "https://jsonplaceholder.typicode.com/posts";
+const options = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json; charset=utf-8",
+  },
+  body: JSON.stringify({ aula: "JavaScript" }),
+};
+
+fetch(url, options)
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json);
+  });
